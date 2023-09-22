60f342a79367e9a18f68472b95a43c9f759ea289
TiagoKblo <kblotiago@gmail.com>
2023-09-22 10:04:02 -0300

    create exemplos

--- Python/Curso DIO/aula01.py
+++ Python/Curso DIO/aula01.py
@@ -1,3 +1,13 @@
 print("Hello World!")
 print(11+3)
+print("11"+"3")
+nome = 'Tiago'
+idade = 37
+peso = 105.5
+print(nome, idade, peso)
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+peso = input('Qual é o seu peso?')
+print('Olá', nome,'você possui', idade, 'e pesa', peso, 'correto?')
+
 
